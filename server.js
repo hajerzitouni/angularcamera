b158568f0d8b9d45a35f9905c74d5c19f311494f
unknown <hajer.zitouni@esprit.tn>
2021-07-23 16:24:58 +0100

    first commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ANGULARCAMERA'));
+app.use(express.static('./dist/angularCamera'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/ANGULARCAMERA/'}),
+    res.sendFile('index.html', {root: 'dist/angularCamera/'}),
 );
 
 // Start the app by listening on the default Heroku port
